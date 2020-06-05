6a12685050d70fd7d1468d65c724c0c0067e6387
Liam Best <liambbest@googlemail.com>
2020-06-05 16:07:10 +0200

    CV Modal Complete

--- javascript/index.js
+++ javascript/index.js
@@ -6,3 +6,30 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", function () {
   mainNav.classList.toggle("active");
 });
+
+/*MODAL JS*/
+
+const modal = document.getElementById("cv-modal");
+
+const btn = document.getElementById("modal-btn");
+
+const x = document.getElementById("x");
+const close = document.getElementById("close");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+x.onclick = function () {
+  modal.style.display = "none";
+};
+
+close.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
